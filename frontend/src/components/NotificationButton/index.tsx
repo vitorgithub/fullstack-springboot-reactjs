1a63ef66fe46a76d8b45cfb455194d7e42cfb11c
Vitor Luiz <vitorgithub@gmail.com>
2022-11-19 04:45:45 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
-import icon from '../../assets/img/notification-icon.svg'
-import './styles.css'
+import icon from '../../assets/img/notification-icon.svg';
+import './styles.css';
 
 function NotificationButton() {
 
@@ -11,4 +11,4 @@ function NotificationButton() {
 
 }
 
-export default NotificationButton
+export default NotificationButton;
