130837a20c7fdd1a964b635b285638794034a9d1
Vitor Luiz <vitorgithub@gmail.com>
2022-11-19 04:11:03 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="" height="" width="" />
+                <h1>Sistema de Vendas</h1>
+                <p>Desenvolvido por
+                    <a href="https://www.linkedin.com/in/vitorgithub/" target="_blank">linkedin/in/vitorgithub</a>
+                </p>
+            </div>
+        </header>
+    )
+
+}
+
+export default Header
---
+++ frontend/src/components/SalesCard/index.tsx
@@ -0,0 +1,81 @@
+import NotificationButton from '../NotificationButton'
+import './styles.css'
+
+function SalesCard() {
+
+    return (
+        <div className="ds-meta-card">
+            <h2 className="dsmeta-sales-title">Vendas</h2>
+            <div>
+                <div className="dsmeta-form-control-container">
+                    <input className="dsmeta-form-control" type="text" />
+                </div>
+                <div className="dsmeta-form-control-container">
+                    <input className="dsmeta-form-control" type="text" />
+                </div>
+            </div>
+
+
+            <div>
+                <table className="dsmeta-sales-table">
+                    <thead>
+                        <tr>
+                            <th><b>ID</b></th>
+                            <th><b>Data</b></th>
+                            <th><b>Vendedor</b></th>
+                            <th><b>Visitas</b></th>
+                            <th><b>Vendas</b></th>
+                            <th><b>Total</b></th>
+                            <th><b>Notificar</b></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>#000</td>
+                            <td>00/00/00</td>
+                            <td>Vendedor001</td>
+                            <td>N° visitas</td>
+                            <td>N° vendas</td>
+                            <td>R$ valor</td>
+                            <td>
+                                <div className="dsmeta-red-button-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>#000</td>
+                            <td>00/00/00</td>
+                            <td>Vendedor002</td>
+                            <td>N° visitas</td>
+                            <td>N° vendas</td>
+                            <td>R$ valor</td>
+                            <td>
+                                <div className="dsmeta-red-button-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>#000</td>
+                            <td>00/00/00</td>
+                            <td>Vendedor003</td>
+                            <td>N° visitas</td>
+                            <td>N° vendas</td>
+                            <td>R$ valor</td>
+                            <td>
+                                <div className="dsmeta-red-button-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody >
+                </table >
+            </div >
+
+        </div >
+
+    )
+}
+
+export default SalesCard
