69ad34fc68e7d9b840411c0170d38776799446cf
Vitor Luiz <vitorgithub@gmail.com>
2022-11-21 20:48:15 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -21,11 +21,17 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`)
+
+        const dMin = minDate. toISOString().slice(0, 10);
+        const dMax = maxDate. toISOString().slice(0, 10);
+        console.log(dMin);
+        console.log(dMax);
+
+        axios.get(`${BASE_URL}/sales/?minDate=${dMin}&maxDate=${dMax}`)
             .then(response => {
                 setSales(response.data.content);
             });
-    }, []);
+    }, [minDate, maxDate]);
 
     return (
         <div className="ds-meta-card">
